b4ad1b2d4e46ed5a52ede086fcbcf111ad61d032
Maksym Boiko <maksym782@gmail.com>
2025-01-28 16:51:50 +0100

    patch

--- src/migrations-utils/db.ts
+++ src/migrations-utils/db.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 
 export const getDb = async () => {
   await dotenv.config();
-  const MONGO_URL = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@localhost:${process.env.MONGO_PORT}/`;
+  const MONGO_URL = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/`;
   const client: any = await MongoClient.connect(MONGO_URL, {});
   return client.db('nest');
 };
